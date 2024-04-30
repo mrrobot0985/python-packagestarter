0182fdf0959e4f5b434f8d72b039bb3fa5ca38cc
mrrobot0985 <164061086+mrrobot0985@users.noreply.github.com>
2024-04-30 23:01:48 +0000

    Remove poetry.lock file test

--- tests/test_self.py
+++ tests/test_self.py
@@ -1,61 +1,77 @@
 import os
 
+
 def test_license_exists():
     assert os.path.exists("LICENSE")
+
 
 def test_makefile_exists():
     assert os.path.exists("Makefile")
 
+
 def test_readme_exists():
     assert os.path.exists("README.md")
 
-def test_poetry_lock_exists():
-    assert os.path.exists("poetry.lock")
 
 def test_pyproject_toml_exists():
     assert os.path.exists("pyproject.toml")
 
+
 def test_src_directory_exists():
     assert os.path.exists("src")
+
 
 def test_tests_directory_exists():
     assert os.path.exists("tests")
 
+
 def test_docs_directory_exists():
     assert os.path.exists("docs")
+
 
 def test_base_templates_md_exists():
     assert os.path.exists("docs/base_templates.md")
 
+
 def test_git_initializer_md_exists():
     assert os.path.exists("docs/git_initializer.md")
+
 
 def test_packagestarter_md_exists():
     assert os.path.exists("docs/packagestarter.md")
 
+
 def test_structure_creation_md_exists():
     assert os.path.exists("docs/structure_creation.md")
+
 
 def test_packagestarter_directory_exists():
     assert os.path.exists("src/packagestarter")
 
+
 def test_package_init_py_exists():
     assert os.path.exists("src/packagestarter/__init__.py")
+
 
 def test_base_templates_py_exists():
     assert os.path.exists("src/packagestarter/base_templates.py")
 
+
 def test_git_initializer_py_exists():
     assert os.path.exists("src/packagestarter/git_initializer.py")
+
 
 def test_packagestarter_py_exists():
     assert os.path.exists("src/packagestarter/packagestarter.py")
 
+
 def test_structure_creation_py_exists():
     assert os.path.exists("src/packagestarter/structure_creation.py")
 
+
 def test_templates_directory_exists():
     assert os.path.exists("src/packagestarter/templates")
+
 
 def test_templates_files_exist():
     base_path = "src/packagestarter/templates"
@@ -71,6 +87,7 @@ def test_templates_files_exist():
     assert os.path.exists(os.path.join(base_path, "docs/index.md.jinja"))
     assert os.path.exists(os.path.join(base_path, "docs/BUILD.md.jinja"))
     assert os.path.exists(os.path.join(base_path, "root/Makefile.jinja"))
+
 
 def test_self_py_exists():
     assert os.path.exists("tests/test_self.py")
